fcce0ae9a4d7cf8f03ec410f34509303d5a8e7f3
MM0199 <pn420360@gmail.com>
2024-10-09 17:16:30 -0500

    Added decoder with sample for test

--- Main.java
+++ Main.java
@@ -1,13 +1,26 @@
-import javax.swing.*;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
+        decoder();
+    }
 
-        SwingUtilities.invokeLater(() -> {
-            DecodeUI frame = new DecodeUI();
-            frame.setVisible(true);
-        });
+    private static void decoder() {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter the image name: ");
+        String path = keyboard.next();
+
+        try {
+            BufferedImage image = ImageIO.read(new File(path));
+
+            System.out.println("Hidden Message: " + LSBDecoder.extractMessage(image));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
